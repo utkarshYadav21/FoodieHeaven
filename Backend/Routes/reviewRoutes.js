53988597c14507d8648bfde81cddc9a045d13097
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-09-10 14:17:57 +0530

    update:backend seperated

--- Backend/Routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const reviewController=require("../Controllers/reviewController")
-
-router.post("/post/:resId",reviewController.review_post);
-// router.get("/get",reviewController.review_get)
-
-module.exports=router;
